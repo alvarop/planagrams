0881bb04b65c984c6d0ab08b42f17f972df092d5
Alvaro Prieto <apg88zx@gmail.com>
2015-08-27 16:58:52 -0700

    Split into separate lists

--- nagaram.py
+++ nagaram.py
@@ -6,15 +6,23 @@ WORDFILE = '/usr/share/dict/words'
 
 try:
 	with open(WORDFILE) as wordList:
-		letterCount = {}
+		
+		# Split into lists of words with the same number of letters
+		letterLists = {}
+		
 		for word in wordList:
 			wordLen = len(word.strip())
-			if wordLen in letterCount:
-				letterCount[wordLen] += 1
-			else:
-				letterCount[wordLen] = 1
 
-	print letterCount
+			if wordLen < 4:
+				continue
+
+			if wordLen in letterLists:
+				letterLists[wordLen].append(word.strip())
+			else:
+				letterLists[wordLen] = []
+				letterLists[wordLen].append(word.strip())
+
+	# print letterLists
 except:
 	print('Could not open wordfile \'' + WORDFILE + '\'')
 
